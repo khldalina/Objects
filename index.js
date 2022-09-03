9000d4da6ab68f3c09c4e98d0ffb69e59eb33417
Alina <khldalina@gmail.com>
2022-09-03 18:32:30 +0300

    added new code

---
+++ index.js
@@ -0,0 +1,34 @@
+const store = {
+    name: 'Alden',
+    address: {
+        country:'Ukraine',
+        city:'Kiev',
+        street:'Apple',
+        houseNumber:'56',
+    },
+    clients: {
+        'Yuriy Kruchinin': {
+            quantity: 34,
+            bonuses: 16,
+        },
+        'Yvhen Vuzlov':{
+            quantity: 23,
+            bonuses: 11,
+        },
+        'Alina Kholod':{
+            quantity: 19,
+            bonuses: 9,
+        },
+        'Yulia Zavistovska':{
+            quantity: 27,
+            bonuses: 13,
+        },
+        'Bohdan Onatskyi':{
+            quantity: 12,
+            bonuses: 5,
+        },
+    }
+};
+
+
+
